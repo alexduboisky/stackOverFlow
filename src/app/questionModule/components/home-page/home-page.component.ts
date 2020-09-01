832f2cc6b35e37eb54e768d9e4921a9e1a344a4f
alexduboisky <alexduboisky@yandex.ru>
2020-09-01 11:51:14 +0300

    add nav with filters and settings to home-page.component

--- src/app/questionModule/components/home-page/home-page.component.ts
+++ src/app/questionModule/components/home-page/home-page.component.ts
@@ -1,14 +1,16 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../../shared/services/auth.service';
+import * as category from '../../../../assets/data/category.json';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent{
+export class HomePageComponent implements OnInit{
 
   userName: string
+  categoryList: object
 
   constructor(auth: AuthService){
       auth.user.subscribe((user)=>{
@@ -18,6 +20,10 @@ export class HomePageComponent{
       },error => {
         console.log('some errors',error)
       })
+  }
+
+  ngOnInit() {
+    this.categoryList = category.category
   }
 
 
